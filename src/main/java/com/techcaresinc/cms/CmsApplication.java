98e8520175b088f073cd00597366024003e6af8e
JMchfundi <akidamjaffar@gmail.com>
2021-10-05 11:51:00 +0300

    jst sm litle twks for login page

--- src/main/java/com/techcaresinc/cms/CmsApplication.java
+++ src/main/java/com/techcaresinc/cms/CmsApplication.java
@@ -2,7 +2,6 @@ package com.techcaresinc.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CmsApplication extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
-    public String root (){
+    public String root() {
         return "indexlogin";
     }
 
+    @RequestMapping(value = "/admin")
+    public String adminPanel() {
+        return "index";
+    }
+
     @RequestMapping(value = "/form_component")
-    public String form_component(){
+    public String form_component() {
         return "form_component";
     }
 
     @RequestMapping(value = "/form_validation")
-    public String form_validation(){
+    public String form_validation() {
         return "form_validation";
     }
 
